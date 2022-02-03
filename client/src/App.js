eba2127bcace8f63dc28d6b5743b18b196162f94
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-02-03 11:26:20 +0530

    Imported Pages in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+//importing React-Compnents..........
+import { Route, Routes } from "react-router-dom";
+
+//importing pages........
+import AboutUs from "./Pages/AboutUs.js";
+import Blogs from "./Pages/Blogs.js";
+import Coffee from "./Pages/Coffee.js";
+import Machine from "./Pages/Machine.js";
+import Pods from "./Pages/Pods.js";
+import Root from "./Pages/Root.js";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" exact element={<Root />} />
+      <Route path="/pages/about-us" exact element={<AboutUs />} />
+      <Route path="/products/category/coffee" exact element={<Coffee />} />
+      <Route path="/products/category/pods" exact element={<Pods />} />
+      <Route path="/products/category/machine" exact element={<Machine />} />
+      <Route path="/blog" exact element={<Blogs />} />
+    </Routes>
   );
 }
 
